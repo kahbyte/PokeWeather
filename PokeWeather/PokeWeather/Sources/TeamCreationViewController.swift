c35800df6819152d562eacd3e2918c9dd4af9fa5
kahbyte <kaue.sbsousa@gmail.com>
2021-08-09 03:44:09 -0300

    Setting up initial project structure

---
+++ PokeWeather/PokeWeather/Sources/TeamCreationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokeWeather
+//
+//  Created by Kauê Sales on 09/08/21.
+//
+
+import UIKit
+
+class TeamCreationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
+
